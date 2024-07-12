2a85c537a25574beb79b91f5eca5819f28fe6259
Regina Steffen <reginasteffen@live.com>
2024-07-11 20:40:51 -0500

    findFloor() complete

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,24 @@
-function findFloor() {
-  
+function findFloor(arr, val) {
+    // return the floor of a value in an array 
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+    let floor = -1;
+
+    while(leftIdx <= rightIdx){
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+
+        if(arr[middleIdx] === val){
+            return val;
+        }
+        else if(arr[middleIdx] < val){
+            floor = arr[middleIdx];
+            leftIdx = middleIdx + 1;
+        }
+        else{
+            rightIdx = middleIdx - 1;
+        }
+    }
+    return floor; 
 }
 
 module.exports = findFloor
